f8d6b91d658b8d0c77474e812d3b7e6fc06d7440
entername1983 <kevin.e.mccarthy1983@gmail.com>
2024-11-28 16:23:24 +0000

    added fish and i3 files

---
+++ config.fish
@@ -0,0 +1,10 @@
+# ~/.config/fish/config.fish
+
+# Set locale to UTF-8
+set -x LANG en_US.UTF-8
+set -x LANGUAGE en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
+
+# Initialize Starship prompt
+starship init fish | source
+
