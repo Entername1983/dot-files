3b5ac5425932850a6c0f48d6bf8bde5a2ebca3b2
Kevin <kevin.e.mccarthy1983@gmail.com>
2024-12-30 09:50:48 +0000

    fixed config.fish

--- config.fish
+++ config.fish
@@ -1,10 +1,17 @@
-# ~/.config/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
-# Set locale to UTF-8
-set -x LANG en_US.UTF-8
-set -x LANGUAGE en_US.UTF-8
-set -x LC_ALL en_US.UTF-8
 
-# Initialize Starship prompt
+
+
+alias ls 'eza -l --icons=always --color=always --hyperlink'
+alias my-desktop="ssh -6 fe80::d4d8:175e:6121:3212%wlp2s0"
+
+neofetch
+
 starship init fish | source
+
+tmux
+
 
